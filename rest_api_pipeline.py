fea47881d7a00a51cba45b4095a9923047e7a5b5
Zach Mandell <zmandell@umich.edu>
2024-10-18 16:02:51 -0400

    adding stuff

--- rest_api_pipeline.py
+++ rest_api_pipeline.py
@@ -1,158 +1,65 @@
 from typing import Any, Optional
+import os
+from datetime import datetime, timedelta
 
 import dlt
-from dlt.common.pendulum import pendulum
-from dlt.sources.rest_api import (
-    RESTAPIConfig,
-    check_connection,
-    rest_api_resources,
-    rest_api_source,
-)
+from dlt.sources.rest_api import rest_api_source, RESTAPIConfig
 
+# Function to get the date range for the API call
+def get_date_range():
+    end_date = datetime.now().strftime("%Y-%m-%d")
+    start_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    return start_date, end_date
 
-@dlt.source(name="github")
-def github_source(access_token: Optional[str] = dlt.secrets.value) -> Any:
-    # Create a REST API configuration for the GitHub API
-    # Use RESTAPIConfig to get autocompletion and type checking
+@dlt.source(name="evocon")
+def evocon_source(api_key: Optional[str] = dlt.secrets.value) -> Any:
+    start_date, end_date = get_date_range()
+    
     config: RESTAPIConfig = {
         "client": {
-            "base_url": "https://api.github.com/repos/dlt-hub/dlt/",
-            # we add an auth config if the auth token is present
-            "auth": (
-                {
-                    "type": "bearer",
-                    "token": access_token,
-                }
-                if access_token
-                else None
-            ),
+            "base_url": "https://api.evocon.com/api/reports/",
+            "auth": {
+                "type": "basic",
+                "username": dlt.secrets.sources.evocon.api_key,
+                "password": dlt.secrets.sources.evocon.secret,
+            },
         },
-        # The default configuration for all resources and their endpoints
         "resource_defaults": {
-            "primary_key": "id",
             "write_disposition": "merge",
             "endpoint": {
                 "params": {
-                    "per_page": 100,
+                    "startTime": start_date,
+                    "endTime": end_date,
                 },
             },
         },
         "resources": [
-            # This is a simple resource definition,
-            # that uses the endpoint path as a resource name:
-            # "pulls",
-            # Alternatively, you can define the endpoint as a dictionary
-            # {
-            #     "name": "pulls", # <- Name of the resource
-            #     "endpoint": "pulls",  # <- This is the endpoint path
-            # }
-            # Or use a more detailed configuration:
             {
-                "name": "issues",
+                "name": "oee",
                 "endpoint": {
-                    "path": "issues",
-                    # Query parameters for the endpoint
-                    "params": {
-                        "sort": "updated",
-                        "direction": "desc",
-                        "state": "open",
-                        # Define `since` as a special parameter
-                        # to incrementally load data from the API.
-                        # This works by getting the updated_at value
-                        # from the previous response data and using this value
-                        # for the `since` query parameter in the next request.
-                        "since": {
-                            "type": "incremental",
-                            "cursor_path": "updated_at",
-                            "initial_value": pendulum.today().subtract(days=30).to_iso8601_string(),
-                        },
-                    },
+                    "path": "oee_json",
                 },
             },
-            # The following is an example of a resource that uses
-            # a parent resource (`issues`) to get the `issue_number`
-            # and include it in the endpoint path:
             {
-                "name": "issue_comments",
+                "name": "losses",
                 "endpoint": {
-                    # The placeholder {issue_number} will be resolved
-                    # from the parent resource
-                    "path": "issues/{issue_number}/comments",
-                    "params": {
-                        # The value of `issue_number` will be taken
-                        # from the `number` field in the `issues` resource
-                        "issue_number": {
-                            "type": "resolve",
-                            "resource": "issues",
-                            "field": "number",
-                        }
-                    },
+                    "path": "losses_json",
                 },
-                # Include data from `id` field of the parent resource
-                # in the child data. The field name in the child data
-                # will be called `_issues_id` (_{resource_name}_{field_name})
-                "include_from_parent": ["id"],
             },
         ],
     }
 
-    yield from rest_api_resources(config)
+    yield from rest_api_source(config)
 
-
-def load_github() -> None:
+def load_evocon_data() -> None:
     pipeline = dlt.pipeline(
-        pipeline_name="rest_api_github",
+        pipeline_name="evocon_pipeline",
         destination='snowflake',
-        dataset_name="rest_api_data",
+        dataset_name="evocon_data",
     )
 
-    load_info = pipeline.run(github_source())
-    print(load_info)  # noqa: T201
-
-
-def load_pokemon() -> None:
-    pipeline = dlt.pipeline(
-        pipeline_name="rest_api_pokemon",
-        destination='snowflake',
-        dataset_name="rest_api_data",
-    )
-
-    pokemon_source = rest_api_source(
-        {
-            "client": {
-                "base_url": "https://pokeapi.co/api/v2/",
-                # If you leave out the paginator, it will be inferred from the API:
-                # "paginator": "json_link",
-            },
-            "resource_defaults": {
-                "endpoint": {
-                    "params": {
-                        "limit": 1000,
-                    },
-                },
-            },
-            "resources": [
-                "pokemon",
-                "berry",
-                "location",
-            ],
-        }
-    )
-
-    def check_network_and_authentication() -> None:
-        (can_connect, error_msg) = check_connection(
-            pokemon_source,
-            "not_existing_endpoint",
-        )
-        if not can_connect:
-            pass  # do something with the error message
-
-    check_network_and_authentication()
-
-    load_info = pipeline.run(pokemon_source)
-    print(load_info)  # noqa: T201
-
+    load_info = pipeline.run(evocon_source())
+    print(load_info)
 
 if __name__ == "__main__":
-    load_github()
-    load_pokemon()
+    load_evocon_data()
